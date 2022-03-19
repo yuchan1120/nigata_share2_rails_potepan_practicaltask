30d2ae1ddab9cc7c7119e65c2a48e835cd0c48e9
Yuki MIURA <nogginpanic@gmail.com>
2022-03-20 06:56:52 +0900

    「アカウント登録」機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
+
+  devise_scope :user do
+    get "user/:id", :to => "users/registrations#detail"
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+  end
   root 'top#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

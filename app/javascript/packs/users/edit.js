8c30e6644af426e9ae44351604c451bbaf4d9695
Yuki MIURA <nogginpanic@gmail.com>
2022-03-21 21:54:54 +0900

    「アカウント情報編集」機能を修正する

--- app/javascript/packs/users/edit.js
+++ app/javascript/packs/users/edit.js
@@ -1,5 +1,4 @@
 function hamburger() {
-  document.getElementById('line1').classList.toggle('line_1');
   document.getElementById('nav').classList.toggle('in');
 }
 document.getElementById('hamburger').addEventListener('click' , function () {

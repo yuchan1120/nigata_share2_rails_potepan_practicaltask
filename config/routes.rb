ddff7c98cc267bacc4cf15353e85bead3374bc39
Yuki MIURA <nogginpanic@gmail.com>
2022-03-21 15:12:06 +0900

    「ルーム検索」機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
     get 'users/show', :to => 'users/registrations#show'
   end
   root 'top#index'
+  get 'search' => 'rooms#search'
+  get 'area' => 'rooms#area'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
